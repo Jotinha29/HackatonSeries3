8cbfa9fec5a2d316d34bcb6fc40fd32499b590de
João Pedro Alcântara P. Calzá <112181440+Jotinha29@users.noreply.github.com>
2024-06-30 19:30:54 -0300

    Update main.py

--- Desafio 1/main.py
+++ Desafio 1/main.py
@@ -1,15 +1,12 @@
 from read_data import coletar_dados_camara, coletar_dados_senado
-from data_to_DB import salvar_dados_no_banco
+from data_to_DB import gerar_sql
 
 def main():
-    # Coletando dados da Câmara dos Deputados
     df_camara = coletar_dados_camara()
-    # Coletando dados do Senado Federal
     df_senado = coletar_dados_senado()
     
-    # Salvando dados no banco de dados
-    salvar_dados_no_banco(df_camara, 'proposicoes_camara')
-    salvar_dados_no_banco(df_senado, 'proposicoes_senado')
+    gerar_sql(df_camara, 'proposicoes_camara.sql', 'proposicoes_camara')
+    gerar_sql(df_senado, 'proposicoes_senado.sql', 'proposicoes_senado')
 
 if __name__ == "__main__":
     main()
